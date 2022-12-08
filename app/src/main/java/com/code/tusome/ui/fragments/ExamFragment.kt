c62e4f02c63deb856595e69014c62458659ce1f9
Jamie Omondi <cruiseomondi90@gmail.com>
2022-12-08 13:44:36 +0300

    More viewmodels and fragments config

--- app/src/main/java/com/code/tusome/ui/fragments/ExamFragment.kt
+++ app/src/main/java/com/code/tusome/ui/fragments/ExamFragment.kt
@@ -5,15 +5,17 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.code.tusome.R
 import com.code.tusome.databinding.FragmentExamBinding
-import com.code.tusome.ui.viewmodels.MainViewModel
+import com.code.tusome.models.Course
+import com.code.tusome.ui.viewmodels.ExamViewModel
 
 class ExamFragment : Fragment() {
     private lateinit var binding:FragmentExamBinding
-    private val mainViewModel:MainViewModel by viewModels()
+    private val examViewModel:ExamViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i(TAG, "onCreate: Fragment started successfully")
@@ -21,7 +23,17 @@ class ExamFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mainViewModel.get
+        examViewModel.getExams(Course("","", ArrayList(),"","")).observe(viewLifecycleOwner){
+            if (it!!.isEmpty()){
+                binding.emptyBoxIv.visibility = VISIBLE
+                binding.emptyBoxTv.visibility = VISIBLE
+                binding.examsRecycler.visibility = GONE
+            }else{
+                binding.emptyBoxIv.visibility = GONE
+                binding.emptyBoxTv.visibility = GONE
+                binding.examsRecycler.visibility = VISIBLE
+            }
+        }
     }
 
     override fun onCreateView(
